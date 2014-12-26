c5c472a0c69eab65594763a48b9a0d3611cae783
michaelalei <michaeltb@163.com>
2014-12-26 16:42:58 +0800

    Initial Commit

---
+++ AudioPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioPlayer
+//
+//  Created by qianfeng on 14-12-26.
+//  Copyright (c) 2014年 Qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
